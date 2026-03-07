50dd0f308ecf7b0711350d4ce6dfc003f4fdbfde
Serhat Marangoz <serhatmar05@gmail.com>
2026-03-07 17:13:01 +0100

    Zwischen Commit

--- scenes/FireTruck.gd
+++ scenes/FireTruck.gd
@@ -41,4 +41,4 @@ func _win_level():
 	# play sound
 	
 	await get_tree().create_timer(1.0).timeout
-	get_tree().change_scene_to_file("res://NextLevel.tscn")
+	#get_tree().change_scene_to_file("res://NextLevel.tscn")
